63bbb888cddf0349c5add3210d364560e3a23dfd
Zinnocent <zinnocent2025@gmail.com>
2026-07-12 18:05:31 +0100

    Enable debug temporarily

--- hostel_system/settings.py
+++ hostel_system/settings.py
@@ -138,8 +138,9 @@ ASGI_APPLICATION = 'hostel_system.asgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
